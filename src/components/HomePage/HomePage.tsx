4166bfbddaf29c5be19f52625bb2378395df8ffa
Amir Basirati <amirbasirati13@gmail.com>
2024-02-20 15:23:51 +0330

    update: landing button scrolls to store 🔧

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,16 +1,36 @@
+"use client";
 import { Product } from "@/types/product";
 import Landing from "./landing/Landing";
 import Shop from "./shop/Shop";
+import { useEffect, useRef, useState } from "react";
 
 interface Props {
   products: Product[];
 }
 
 const HomePage: React.FC<Props> = ({ products }) => {
+  const [activeSection, setActiveSection] = useState(null);
+
+  const initRef = useRef<HTMLDivElement>(null);
+
+  const handleScroll = () => {
+    if (initRef.current) {
+      initRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div>
-      <Landing />
-      <Shop />
+      <Landing handleScroll={handleScroll} />
+      <div ref={initRef}>
+        <Shop />
+      </div>
     </div>
   );
 };
--- src/components/HomePage/landing/Landing.tsx
+++ src/components/HomePage/landing/Landing.tsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 import Carousel from "./carousel/Carousel";
 import intro from "@/public/intro.svg";
 
-const Landing = () => {
+interface Props {
+  handleScroll: Function;
+}
+
+const Landing: React.FC<Props> = ({ handleScroll }) => {
   return (
     <div className=" w-full h-[100dvh] bg-shopBlue dark:bg-shopBlueD flex flex-col items-center gap-y-8">
       <div className="mt-16">
@@ -10,7 +14,10 @@ const Landing = () => {
       </div>
       <h1 className="text-white text-6xl m text-center">WEB SHOP</h1>
       <Carousel />
-      <button className="bg-purple-700 dark:bg-purple-500 text-white text-4xl p-2 rounded-2xl mt-6 font-rubik transition-shadow duration-100 ease-linear hover:shadow-2xl hover:shadow-black">
+      <button
+        onClick={() => handleScroll()}
+        className="bg-purple-700 dark:bg-purple-500 text-white text-4xl p-2 rounded-2xl mt-6 font-rubik transition-shadow duration-100 ease-linear hover:shadow-2xl hover:shadow-black"
+      >
         STORE
       </button>
     </div>
