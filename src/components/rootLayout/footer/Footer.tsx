25f85f3c6404ece40fc71a61f48540baa7956fb7
Amir Basirati <amirbasirati13@gmail.com>
2024-02-20 15:28:26 +0330

    new: footer created ✅

--- src/components/rootLayout/footer/Footer.tsx
+++ src/components/rootLayout/footer/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className=" bg-shopBlue w-full py-2">
+      <p className=" text-gray-300 text-center">WEB SHOP 2024 | Amirbt13</p>
+    </div>
+  );
 };
 
 export default Footer;
