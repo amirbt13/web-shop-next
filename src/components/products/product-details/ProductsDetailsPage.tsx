7ceaa2fd1ce2fbb5986b2cb490d38bf397e598dd
Amir Basirati <amirbasirati13@gmail.com>
2024-02-23 19:39:07 +0330

    update: product details page responsive desktop views 🔨

--- src/components/products/product-details/ProductsDetailsPage.tsx
+++ src/components/products/product-details/ProductsDetailsPage.tsx
@@ -17,20 +17,20 @@ const ProductsDetailsPage: React.FC<Props> = ({ product }) => {
     rating: { rate, count },
   } = product;
   return (
-    <div className=" min-h-[100dvh]">
-      <div className=" bg-white flex items-center justify-center">
+    <div className=" h-[calc(100dvh-85px)] lg:flex lg:flex-row-reverse">
+      <div className=" bg-white flex items-center justify-center lg:basis-2/3">
         <Image
           src={image}
           alt={title}
           width={400}
           height={500}
           quality={50}
-          className=" w-5/6 "
+          className=" w-1/2 "
         />
       </div>
-      <div className=" pt-2 pb-12 px-4">
+      <div className=" pt-2 pb-12 px-4 lg:basis-1/3">
         <div className=" flex justify-between mb-4 mt-2 items-start">
-          <div className="flex flex-col gap-y-2">
+          <div className="flex flex-col gap-y-2 lg:gap-y-6">
             <RatingStar rate={rate} />
             <p className=" text-white bg-shopBlue dark:bg-shopBlueD py-1 px-2 rounded dark:border dark:border-gray-500">
               {category}
@@ -41,7 +41,7 @@ const ProductsDetailsPage: React.FC<Props> = ({ product }) => {
         <div>
           <p className=" dark:text-white">{description}</p>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center lg:mt-10">
           <button className=" bg-purple-700 dark:bg-purple-500 text-white flex items-center gap-2 py-1 px-2 rounded-md mt-4">
             ADD TO CART{" "}
             <Image src={cartPlus} alt="cart" width={30} height={30} />
