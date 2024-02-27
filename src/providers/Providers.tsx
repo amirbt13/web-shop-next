a780d8b9d0e9b1fca52cf26dda4b4d3ff0216211
Amir Basirati <amirbasirati13@gmail.com>
2024-02-27 20:20:10 +0330

    update: sessionProvider added to providers for nextauth authentication🔨

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -3,15 +3,18 @@ import store from "@/redux/store";
 import SyncCart from "@/utils/SyncCart";
 import SyncDarkMode from "@/utils/SyncDarkMode";
 import SyncFavorites from "@/utils/SyncFavorites";
+import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      <SyncDarkMode />
-      <SyncCart />
-      <SyncFavorites />
-      {children}
+      <SessionProvider>
+        <SyncDarkMode />
+        <SyncCart />
+        <SyncFavorites />
+        {children}
+      </SessionProvider>
     </Provider>
   );
 }
