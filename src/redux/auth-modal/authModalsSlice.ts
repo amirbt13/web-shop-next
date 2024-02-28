042be7b7335ba5f2801deadcfb96e38115197864
Amir Basirati <amirbasirati13@gmail.com>
2024-02-28 16:44:10 +0330

    update: authModal slice has a var that represents showing modal🔨

--- src/redux/auth-modal/authModalsSlice.ts
+++ src/redux/auth-modal/authModalsSlice.ts
@@ -3,11 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 export interface AuthModals {
   signup: boolean;
   signin: boolean;
+  showModal: boolean;
 }
 
 const initialState: AuthModals = {
-  signup: false,
+  signup: true,
   signin: false,
+  showModal: true,
 };
 
 const authModalsSlice = createSlice({
@@ -15,15 +17,21 @@ const authModalsSlice = createSlice({
   initialState,
   reducers: {
     showSignup: (state) => {
+      state.showModal = true;
+      state.signin = false;
       state.signup = true;
     },
     hideSignup: (state) => {
+      state.showModal = false;
       state.signup = false;
     },
     showSignin: (state) => {
+      state.showModal = true;
+      state.signup = false;
       state.signin = true;
     },
     hideSingin: (state) => {
+      state.showModal = false;
       state.signin = false;
     },
   },
