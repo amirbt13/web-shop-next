f73791e12452bd7910163f13b2834c043857135f
Amir Basirati <amirbasirati13@gmail.com>
2024-03-02 16:33:13 +0330

    update: store gets products form next api now🔨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+import { ProductType } from "@/types/product";
+import { Products } from "@/types/products";
 import catchLog from "@/utils/catchLog";
 import HomePage from "src/components/HomePage/HomePage";
 
 export default async function Home() {
   try {
-    const res = await fetch(`${process.env.BASE_URL}/products`);
-    const data = await res.json();
-
-    return <HomePage products={data} />;
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/products`,
+      {
+        cache: "no-store",
+      }
+    );
+    const data: Products = await res.json();
+    console.log(data);
+    return <HomePage products={data.products} />;
   } catch (err) {
     catchLog("app/page.tsx", err);
   }
