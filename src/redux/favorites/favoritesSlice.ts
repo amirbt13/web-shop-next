a7323d0958f788b75fe8f48fee7d76c408ce9e10
Amir Basirati <amirbasirati13@gmail.com>
2024-03-04 16:31:45 +0330

    fixed: syncing favorites between store and localstorage issue fixed 🕷️

--- src/redux/favorites/favoritesSlice.ts
+++ src/redux/favorites/favoritesSlice.ts
@@ -3,10 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export interface Favorites {
   items: ProductType[];
+  init: boolean;
 }
 
 const initialState: Favorites = {
   items: [],
+  init: true,
 };
 
 const favoritesSlice = createSlice({
@@ -15,13 +17,20 @@ const favoritesSlice = createSlice({
   reducers: {
     addToFaves: (state, action: { type: string; payload: ProductType }) => {
       state.items.push(action.payload);
+      state.init = false;
     },
     removeFromFaves: (state, action: { type: string; payload: number }) => {
       const newItems = state.items.filter((item) => item.id !== action.payload);
       state.items = [...newItems];
+      state.init = false;
+    },
+    insertBulk: (state, action: { type: string; payload: ProductType[] }) => {
+      state.items = [...action.payload];
+      state.init = false;
     },
   },
 });
 
-export const { addToFaves, removeFromFaves } = favoritesSlice.actions;
+export const { addToFaves, removeFromFaves, insertBulk } =
+  favoritesSlice.actions;
 export default favoritesSlice.reducer;
--- src/utils/SyncFavorites.tsx
+++ src/utils/SyncFavorites.tsx
@@ -1,13 +1,28 @@
 "use client";
 
+import { insertBulk } from "@/redux/favorites/favoritesSlice";
 import { Store } from "@/types/store/storeType";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 
 const SyncFavorites = () => {
-  const favorites = useSelector((state: Store) => state.favorites.items);
+  const favorites = useSelector((state: Store) => state.favorites);
+  const dispatch = useDispatch();
   useEffect(() => {
-    localStorage.setItem("faves", JSON.stringify(favorites));
+    const faveItems = localStorage.getItem("faves");
+    if (!faveItems) {
+      return;
+    }
+    const parsedFavesArr = JSON.parse(faveItems);
+    dispatch(insertBulk(parsedFavesArr));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (favorites.init) {
+      return;
+    }
+    localStorage.setItem("faves", JSON.stringify(favorites.items));
   }, [favorites]);
   return null;
 };
